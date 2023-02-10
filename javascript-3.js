a7aa322dfa58c2351b40fc8059700b916ac0f25f
Ethan Gaebel <2490298+egaebel@users.noreply.github.com>
2023-02-09 16:02:43 -0800

    Add tests.

--- javascript-3.js
+++ javascript-3.js
@@ -23,6 +23,7 @@ function stringLength(s) {
 // Input: "this is a string"
 // Output: ["t", "h" "i", "s"," ", "i", "s"," ", "a", " ", "s", "t", "r", "i", "n", "g"]
 //
+function stringToArray(s) { }
 
 
 // 3. Join character array.
@@ -33,6 +34,7 @@ function stringLength(s) {
 // Input: ["t", "h", "i", "s"," ", "i", "s"," ", "a", " ", "s", "t", "r", "i", "n", "g"]
 // Output: "this is a string"
 //
+function arrayToString(charArray) { }
 
 
 // 4. Take a substring from a string.
@@ -64,6 +66,7 @@ function takeSubstring(s, startIndex, endIndex) {
 // Input: [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384]
 // Output: 32,767
 //
+function sumNum(numArray) { }
 
 
 // 6. Printing even numbers (modulus operator) (example of usefulness on audio.news).
@@ -77,7 +80,10 @@ function takeSubstring(s, startIndex, endIndex) {
 // Reference:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder
+//
+function printEven(numArray) { }
 
+function getEven(numArray) { }
 
 // 7. Fizz Buzz.
 // When given a number, n, count up to that number from 1 and 
@@ -103,4 +109,38 @@ function takeSubstring(s, startIndex, endIndex) {
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder
 //
+function fizzBuzz(n) { }
 
+// Testing solutions-------------------------------------------------------------
+console.log("\n1. String length")
+let ret = stringLength("this is a string");
+console.log("stringLength(\"this is a string\"): " + ret);
+
+console.log("\n2. String/character array comparison.")
+ret = stringToArray("this is a string");
+console.log("stringToArray(\"this is a string\"): " + ret);
+
+console.log("\n3. Join character array.")
+ret = arrayToString(['t', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 's', 't', 'r', 'i', 'n', 'g']);
+console.log("arrayToString(['t', 'h' 'i', 's',' ', 'i', 's',' ', 'a', ' ', 's', 't', 'r', 'i', 'n', 'g']): " + ret);
+
+console.log("\n4. Take a substring from a string.")
+ret = takeSubstring("this is a string", 0, 4);
+console.log("takeSubstring(\"this is a string\", 0, 4): " + ret);
+ret = takeSubstring("this is a string", 4, 9);
+console.log("takeSubstring(\"this is a string\", 4, 9): " + ret);
+ret = takeSubstring("this is a string", 8, 16);
+console.log("takeSubstring(\"this is a string\", 8, 16): " + ret);
+
+console.log("\n5. Summing numbers.")
+ret = sumNum([1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384]);
+console.log("sumNum([1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384]): " + ret);
+
+console.log("\n6. Printing even numbers (modulus operator).")
+printEven([-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]);
+ret = getEven([-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]);
+console.log("getEven([-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]): " + ret);
+
+console.log("\n7. Fizz Buzz.")
+ret = fizzBuzz(16);
+console.log("fizzBuzz(16): " + ret);
